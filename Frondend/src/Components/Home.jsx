c7aa8589062504b732cbd22231254381ef3fe1a0
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-21 19:01:33 +0530

    sent request to server

--- Frondend/src/Components/Home.jsx
+++ Frondend/src/Components/Home.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../public/Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const [userName, setUserName] = useState("");
+  const navigate = useNavigate();
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    localStorage.setItem("userName", userName);
+    navigate("/chat");
+  };
+
   return (
-    <form className="home-container">
+    <form className="home-container" onSubmit={handleInput}>
       <h2 className="home-header">Sign in to chat</h2>
       <label htmlFor="username">Username</label>
       <input
@@ -12,6 +22,8 @@ const Home = () => {
         id="username"
         className="username-input"
         placeholder="Type here"
+        onChange={(event) => setUserName(event.target.value)}
+        value={userName}
       />
       <button className="home-btn">Sign in</button>
     </form>
--- Frondend/src/Components/Inbox.jsx
+++ Frondend/src/Components/Inbox.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../public/Inbox.css";
 
-const Inbox = () => {
+const Inbox = ({ socket }) => {
+  const [message, setMessage] = useState("");
+  const handleMessage = (event) => {
+    event.preventDefault();
+    if (message.trim() && localStorage.getItem("userName")) {
+      socket.emit("message", {
+        text: message,
+        name: localStorage.getItem("userName"),
+        id: `${socket.id}${Math.random()}`,
+        socketID: socket.id,
+      });
+    }
+    setMessage("");
+  };
   return (
     <div className="chat-footer">
-      <form className="form">
-        <input type="text" placeholder="Message" className="message" />
+      <form className="form" onSubmit={handleMessage}>
+        <input
+          type="text"
+          placeholder="Message"
+          className="message"
+          onChange={(event) => setMessage(event.target.value)}
+          value={message}
+        />
         <button className="send-btn">
           <i className="fa-solid fa-paper-plane"></i>
         </button>
