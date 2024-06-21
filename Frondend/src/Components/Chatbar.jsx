3d6195e7dd95f5496cd9a2fdfd1c3be6082f6eaf
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-21 20:12:54 +0530

    completed

--- Frondend/src/Components/Chatbar.jsx
+++ Frondend/src/Components/Chatbar.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import '../../public/Chatbar.css'
+import React, { useEffect, useState } from "react";
+import "../../public/Chatbar.css";
 
-const Chatbar = () => {
+const Chatbar = ({ socket }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.on("userResponse", (data) => setUsers(data));
+  }, [socket, users]);
+
   return (
-   <div className="chat-sidebar">
-    <h2>Open chat</h2>
-    <div>
-        <h4 className='chat-headeres'>ACTIVE USERS</h4>
+    <div className="chat-sidebar">
+      <h2>Open chat</h2>
+      <div>
+        <h4 className="chat-headeres">ACTIVE USERS</h4>
         <div className="chat-users">
-            <p>User 1</p>
-            <p>User 2</p>
-            <p>User 3</p>
-            <p>User 4</p>
-            <p>User 5</p>
+          {users.map((user) => (
+            <p key={user.socketID}>{user.userName}</p>
+          ))}
         </div>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default Chatbar
+export default Chatbar;
