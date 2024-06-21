b04589331d7b6b100e02335867aed677f9487a15
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-06-21 17:27:36 +0530

    ui completed

--- Frondend/src/Pages/Chatpage.jsx
+++ Frondend/src/Pages/Chatpage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Chatbar from '../Components/Chatbar'
 import '../../public/Chatpage.css'
 import Message from '../Components/Message'
+import Inbox from '../Components/Inbox'
 
 const Chatpage = () => {
   return (
@@ -9,6 +10,7 @@ const Chatpage = () => {
       <Chatbar/>
       <div className="chat-main">
         <Message/>
+        <Inbox/>
       </div>
     </div>
   )
